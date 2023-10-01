ad86e8cab456df6506dbcae201aba398d14d67db
Cengizhan Anıl Onay <cengizhananilonay@gmail.com>
2023-10-01 20:24:51 +0300

    Composer Added

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'anilonay/demo',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '544970ecabb1fbb601a326eef4b8493133e8feba',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'anilonay/demo' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '544970ecabb1fbb601a326eef4b8493133e8feba',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
